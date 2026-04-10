dfc7d9c360adedf066e9a1b91659011ce2d8a07b
niceome <gylim7826@naver.com>
2026-04-10 16:30:16 +0900

    add member, ChatMessage, ChatRoom Entity

--- src/main/java/com/example/TableMate/domain/Member.java
+++ src/main/java/com/example/TableMate/domain/Member.java
@@ -1,4 +1,40 @@
 package com.example.TableMate.domain;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+import java.time.OffsetDateTime;
+
+@Entity
+@Table(name = "member")
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 public class Member {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(nullable = false, name = "member_id")
+    private Long id;
+
+    @Column(nullable = false, name = "member_email")
+    private String memberEmail;
+
+    @Column(nullable = false, name = "member_password")
+    private String memberPassword;
+
+    @Column(nullable = false, name = "member_nickname")
+    private String nickname;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false, name = "member_liking")
+    private Liking liking;
+
+    @Column(nullable = false, name = "created_at")
+    private OffsetDateTime createdAt;
 }
