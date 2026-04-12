ddb88e3cca6118a31d2792b8301d6a1cf3f30e2f
noireed <askqwerty54@gmail.com>
2026-04-12 12:36:39 +0900

    Create Post.java

---
+++ src/main/java/com/example/TableMate/entity/Post.java
@@ -0,0 +1,47 @@
+package com.example.tablemate.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "posts")
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
+@Builder
+public class Post {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "post_id")
+    private Long id;
+
+    @Column(name = "post_title")
+    private String title;
+
+    @Column(name = "post_category")
+    private String category;
+
+    @Column(name = "post_due")
+    private LocalDateTime dueTime;
+
+    @Column(name = "post_party")
+    private Long party;
+
+    @Column(name = "post_content", columnDefinition = "TEXT")
+    private String content;
+
+    @Column(name = "post_time")
+    private LocalDateTime postTime;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "member_id")
+    private Member writer;
+
+    @PrePersist
+    public void prePersist() {
+        this.postTime = LocalDateTime.now();
+    }
+}
