88a220fbc4ffa9331b82527f2fdff3e4d3e7cdd0
noireed <askqwerty54@gmail.com>
2026-04-12 12:33:12 +0900

    Create TableMateApplication.java

---
+++ src/main/TableMateApplication.java
@@ -0,0 +1,12 @@
+package com.example.tablemate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TableMateApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TableMateApplication.class, args);
+    }
+}
