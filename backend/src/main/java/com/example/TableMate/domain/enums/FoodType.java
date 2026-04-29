ac970b509b72aa766cb29b98f9d8a873bfab9608
niceome <158061511+niceome@users.noreply.github.com>
2026-04-29 15:35:08 +0900

    Delete backend directory

--- backend/src/main/java/com/example/TableMate/domain/enums/FoodType.java
+++
@@ -1,5 +0,0 @@
-package com.example.TableMate.domain.enums;
-
-public enum FoodType {
-    KOREAN, JAPANESE, CHINESE, WESTERN
-}
--- backend/src/main/java/com/example/TableMate/domain/enums/PostStatus.java
+++
@@ -1,5 +0,0 @@
-package com.example.TableMate.domain.enums;
-
-public enum PostStatus {
-    OPEN, CLOSED
-}
