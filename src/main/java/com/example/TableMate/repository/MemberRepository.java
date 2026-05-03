05736231c31f0ebc3c7fff841cced318187f10ff
noireed <askqwerty54@gmail.com>
2026-05-03 10:40:00 +0900

    Update MemberRepository.java

--- src/main/java/com/example/TableMate/repository/MemberRepository.java
+++ src/main/java/com/example/TableMate/repository/MemberRepository.java
@@ -1,10 +1,10 @@
-package com.example.tablemate.repository;
+package com.example.TableMate.repository;
 
-import com.example.tablemate.entity.Member;
+import com.example.TableMate.domain.Member;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.Optional;
 
 public interface MemberRepository extends JpaRepository<Member, Long> {
-    Optional<Member> findByEmail(String email);
+    Optional<Member> findByMemberEmail(String memberEmail);
 }
