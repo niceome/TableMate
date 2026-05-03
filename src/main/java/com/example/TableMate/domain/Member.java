be894bc9c17f9b27893096a630f61d3b9e053de5
noireed <askqwerty54@gmail.com>
2026-05-03 10:39:31 +0900

    Update Member.java

--- src/main/java/com/example/TableMate/domain/Member.java
+++ src/main/java/com/example/TableMate/domain/Member.java
@@ -1,4 +1,40 @@
 package com.example.TableMate.domain;
 
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.time.OffsetDateTime;
+
+@Entity
+@Table(name = "users")
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
+@Builder
 public class Member {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "member_id")
+    private Long memberId;
+
+    @Column(name = "member_email", nullable = false, unique = true)
+    private String memberEmail;
+
+    @Column(name = "member_password", nullable = false)
+    private String memberPassword;
+
+    @Column(name = "member_nickname", nullable = false)
+    private String memberNickname;
+
+    @Column(name = "member_liking", nullable = false)
+    private String memberLiking;
+
+    @Column(name = "created_at")
+    private OffsetDateTime createdAt;
+
+    @PrePersist
+    public void prePersist() {
+        this.createdAt = OffsetDateTime.now();
+    }
 }
