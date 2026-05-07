806e8f22e2f6045c4e6e25bce332e92e954bdd6c
niceome <gylim7826@naver.com>
2026-05-07 12:24:48 +0900

    add swaggerconfig

---
+++ src/main/java/com/example/TableMate/common/config/SwaggerConfig.java
@@ -0,0 +1,24 @@
+package com.example.TableMate.common.config;
+
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.security.SecurityScheme;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@OpenAPIDefinition(
+        info = @Info(
+                title = "TableMate API",
+                description = "학식 밥 친구 매칭 서비스 TableMate REST API 명세서",
+                version = "v1.0.0"
+        )
+)
+@SecurityScheme(
+        name = "bearerAuth",
+        type = SecuritySchemeType.HTTP,
+        scheme = "bearer",
+        bearerFormat = "JWT"
+)
+public class SwaggerConfig {
+}
